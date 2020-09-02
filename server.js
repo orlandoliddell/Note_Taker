ae1d947b8a1a254cad3eb96f9e3c7b5383208f48
filthyningen <malikliddell@gmail.com>
2020-09-02 17:53:22 -0400

    Successful get and ppost of notes.

--- server.js
+++ server.js
@@ -3,24 +3,36 @@ var express = require("express");
 var path = require("path");
 var fs = require("fs");
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3005;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 //The following HTML routes should be created:
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+
 //The following API routes should be created:
 app.get("/api/notes", function(req, res) {
-    return res.json(db);
+    res.json(db);
 });
 app.post("/api/notes", function(req, res) {
-
+    var newNote = req.body;
+    db.push(newNote)
+    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify([...db]), "utf8", function(err){
+        if(err) throw err;
+        res.json([db])
+    })
 });
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
